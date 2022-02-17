437acb94383be3dc5f50422e81b044d499e099b4
Parker Botsford <parker.botsford.dev@gmail.com>
2022-02-16 22:34:04 -0500

    Add todo functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,119 @@ import styles from 'styles/HomePage.module.css';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+import { useState } from 'react';
+
+import { Formik, Form, Field } from 'formik';
+
 const HomePage: NextPage = () => {
+    type Todo = {
+        id: number;
+        text: string;
+        completed: boolean;
+        hidden: boolean;
+    };
+
+    const [todoId, setTodoId] = useState(0);
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const addTodo = (text: string) => {
+        setTodos([
+            ...todos,
+            {
+                id: todoId,
+                text: text,
+                completed: false,
+                hidden: false,
+            },
+        ]);
+
+        setTodoId(todoId + 1);
+    };
+
+    const removeTodo = (todoToRemove: Todo) => {
+        const newTodos = todos.filter((todo) => {
+            return todoToRemove !== todo;
+        });
+
+        setTodos([...newTodos]);
+    };
+
+    const toggleCompleteTodo = (todo: Todo) => {
+        let todosCopy = [...todos];
+        const todoIndex = todosCopy.indexOf(todo);
+        todosCopy[todoIndex].completed = !todosCopy[todoIndex].completed;
+        setTodos([...todosCopy]);
+    };
+
+    const showAllTodos = () => {
+        const newTodos = todos.map((todo) => {
+            todo.hidden = false;
+            return todo;
+        });
+
+        setTodos([...newTodos]);
+    };
+
+    const showActiveTodos = () => {
+        const newTodos = todos.map((todo) => {
+            todo.completed ? (todo.hidden = true) : (todo.hidden = false);
+            return todo;
+        });
+
+        setTodos([...newTodos]);
+    };
+
+    const showCompletedTodos = () => {
+        const newTodos = todos.map((todo) => {
+            todo.completed ? (todo.hidden = false) : (todo.hidden = true);
+            return todo;
+        });
+
+        setTodos([...newTodos]);
+    };
+
+    const removeCompletedTodos = () => {
+        const newTodos = todos.filter((todo) => {
+            return !todo.completed;
+        });
+
+        setTodos([...newTodos]);
+    };
+
+    const todoItems = todos.map((todo) => {
+        return (
+            <li
+                key={todo.id}
+                className={`${styles.todoItem} ${
+                    todo.hidden ? styles.hidden : ''
+                }`}
+            >
+                <button
+                    onClick={() => {
+                        toggleCompleteTodo(todo);
+                    }}
+                >
+                    Complete
+                </button>
+                <p
+                    className={`${styles.todoText} ${
+                        todo.completed ? styles.completed : ''
+                    }`}
+                >
+                    {todo.text}
+                </p>
+                <div className={styles.todoMover}></div>
+                <button
+                    onClick={() => {
+                        removeTodo(todo);
+                    }}
+                >
+                    X
+                </button>
+            </li>
+        );
+    });
+
     return (
         <div className={styles.page}>
             <Head>
@@ -16,23 +128,56 @@ const HomePage: NextPage = () => {
                 <h1 className={styles.headerTitle}>ToDone</h1>
             </header>
             <main className={styles.main}>
-                <section className={styles.addTodoSection}>
-                    <input
-                        type='text'
-                        name=''
-                        id=''
-                        placeholder='Type todo text'
-                        className={styles.todoInput}
-                    />
-                    <button className={styles.addTodoButton}>+</button>
+                <Formik
+                    initialValues={{ todoInput: '' }}
+                    onSubmit={(values, actions) => {
+                        addTodo(values.todoInput);
+                        actions.resetForm();
+                    }}
+                >
+                    <Form className={styles.addTodoSection}>
+                        <Field
+                            id='todoInput'
+                            name='todoInput'
+                            placeholder='Create a new todo...'
+                            className={styles.todoInput}
+                        />
+                        <button type='submit' className={styles.addTodoButton}>
+                            +
+                        </button>
+                    </Form>
+                </Formik>
+                <section className={styles.listOptions}>
+                    <div className={styles.listFilters}>
+                        <p className={styles.filterText}>Show:</p>
+                        <button
+                            onClick={showAllTodos}
+                            className={styles.filterButton}
+                        >
+                            All
+                        </button>
+                        <button
+                            onClick={showActiveTodos}
+                            className={styles.filterButton}
+                        >
+                            Active
+                        </button>
+                        <button
+                            onClick={showCompletedTodos}
+                            className={styles.filterButton}
+                        >
+                            Complete
+                        </button>
+                    </div>
+                    <button
+                        onClick={removeCompletedTodos}
+                        className={styles.clearCompleteButton}
+                    >
+                        Clear Complete
+                    </button>
                 </section>
                 <section className={styles.todoListSection}>
-                    <ul className={styles.todoList}>
-                        <li className={styles.todoItem}>
-                            <button className={styles.todoButton}></button>
-                            <p className={styles.todoText}>Test Todo</p>
-                        </li>
-                    </ul>
+                    <ul className={styles.todoList}>{todoItems}</ul>
                 </section>
             </main>
             <footer className={styles.footer}>
