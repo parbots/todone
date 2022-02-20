adc0bdd88f5423808d912bb0ccab6e960ea31074
Parker Botsford <parker.botsford.dev@gmail.com>
2022-02-19 19:25:22 -0500

    Add structure to AddItemForm component

--- components/AddItemForm/AddItemForm.tsx
+++ components/AddItemForm/AddItemForm.tsx
@@ -1,14 +1,41 @@
+import styles from './AddItemForm.module.css';
 
-import styles from './AddItemForm.module.css'
-import React from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 const AddItemForm = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [isValidName, setValidName] = useState(false);
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setValidName(event.target.value !== '' && event.target.value !== ' ');
+        setInputValue(event.target.value);
+    };
+
+    const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        if (isValidName) {
+            // TODO: create item here
+        }
+
+        setInputValue('');
+    };
+
     return (
-        <div>
-            <h1>AddItemForm</h1>
-        </div>
+        <form onSubmit={handleFormSubmit} className={styles.form}>
+            <input
+                type='text'
+                name='itemInput'
+                placeholder='Add new todo...'
+                value={inputValue}
+                onChange={handleInputChange}
+                className={styles.itemInput}
+            />
+            <button type='submit' className={styles.addItemButton}>
+                Add
+            </button>
+        </form>
     );
 };
 
 export default AddItemForm;
-
